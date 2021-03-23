dd5b1b1821d93d58f8dc39b65a726b297c9fbe27
William Orr <81045133+WilliamJOrrIV@users.noreply.github.com>
2021-03-23 16:08:55 -0400

    Add files via upload
    We can use .NETCORE

---
+++ Inventory-Management-Systems/Inventory-Management-Systems/CustomerModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inventory_Management_Systems
+{
+    class CustomerModel
+    {
+        public string CustomerName { get; set; }
+        public string CustomerAddress { get; set; }
+
+        public string CustomerNumber { get; set; }
+
+
+    }
+}
